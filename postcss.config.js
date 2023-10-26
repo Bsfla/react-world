9814f07c69add14c80e01e56fe95fb1cc936c58a
Bsfla <baebeomsu77@gmail.com>
2023-10-26 18:27:18 +0900

    chore: 세팅

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 export default {
   plugins: {
     tailwindcss: {},
-    autoprefixer: { browsers: ["last 2 versions", "not dead"] },
   },
 };
--- talwind.config.js
+++ talwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      green: '#5cb85c',
+    },
     extend: {},
   },
   plugins: [],
